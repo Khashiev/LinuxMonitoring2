c26ff863936155701f71de166a87469e00f115b6
Khashiev <nxashiev@gmail.com>
2023-10-31 11:29:20 +0300

    fix check part-2

--- src/02/check.sh
+++ src/02/check.sh
@@ -5,18 +5,18 @@ error=0
 function check_input {
     if [[ $# -ne 3 ]]; then
         error=1
-    elif ! [[ $3 =~ ^[a-zA-Z]{1,7}$ ]]; then
+    elif ! [[ $1 =~ ^[a-zA-Z]{1,7}$ ]]; then
         error=2
-    elif ! [[ $5 =~ ^([a-zA-Z+]{1,7})\.([a-zA-Z]{1,3})$ ]]; then
+    elif ! [[ $2 =~ ^([a-zA-Z+]{1,7})\.([a-zA-Z]{1,3})$ ]]; then
         error=3
-    elif [[ $(echo "$6" | awk -F"Mb" '{print $1}') -gt 100 ]]; then
+    elif [[ $(echo "$3" | awk -F"Mb" '{print $1}') -gt 100 ]]; then
         error=4
     fi
 }
 
 function print_error {
     case "$error" in
-    1) echo "You need to specify 6 parameters!" ;;
+    1) echo "You need to specify 3 parameters!" ;;
     2) echo "Incorrect list of letters for dirs names" ;;
     3) echo "Incorrect list of letters for files names" ;;
     4) echo "Incorrect file size" ;;
